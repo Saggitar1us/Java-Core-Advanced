d1286593ef7e0443b196d09f1a0616613f0ec0eb
Aleksey Stepanov <41383880+Saggitar1us@users.noreply.github.com>
2018-09-26 12:20:53 +0300

    Update App.java

--- src/main/java/ru/stepanov/se/oop/App.java
+++ src/main/java/ru/stepanov/se/oop/App.java
@@ -4,20 +4,6 @@ import ru.stepanov.se.oop.competition.Course;
 import ru.stepanov.se.oop.competition.CourseLine;
 import ru.stepanov.se.oop.members.Team;
 
-/**
- *
- *
- *
- *
- * В итоге должно быть что-то вроде:
- *
- * public static void main(String[] args) {
- * Course c = new Course(...); // Создаем полосу препятствий
- * Team team = new Team(...); // Создаем команду
- * c.doIt(team); // Просим команду пройти полосу
- * team.showResults(); // Показываем результаты
- * }
- */
 public class App {
     public static void main(String[] args) {
         Team team = new Team(
